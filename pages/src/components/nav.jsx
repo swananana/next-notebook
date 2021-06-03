aa7b4315071ec205b102766a4900b727f60c6c59
swananana <swananana@users.noreply.github.com>
2021-06-04 01:13:47 +0530

    -okay-doesnt-work

--- pages/src/components/nav.jsx
+++ pages/src/components/nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => (
       <Link href="/notes">
         <a sx={{color: 'text', fontSize: 3, cursor: 'pointer'}}>notes</a>
       </Link>
-      <Link>
+      
       <a sx={{
           color: 'text',
           fontSize: 3,
@@ -21,7 +21,7 @@ const Nav = () => (
       >
         Help
       </a>
-      </Link>
+      
       
 
     </nav>

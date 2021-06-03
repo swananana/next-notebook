2d1095341cd6db077651739434077975d2ee7f80
swananana <swananana@users.noreply.github.com>
2021-06-03 23:58:24 +0530

    -next-notebook-styled

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
-import React from 'react'
 
-export default () => <h1>Index Page</h1>
+/** @jsx jsx */ /** @jsxRuntime classic */ import { jsx } from "theme-ui";
+import Link from 'next/link'
+
+export default () => (
+  <div sx={{ height: `calc(100vh - 60px)`}}>
+    <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
+      <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+    </div>
+  </div> 
+)
